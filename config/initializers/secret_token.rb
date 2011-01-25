2d486b3d99eb8b0def92362491952c4f03c533c2
Niklas Granö <niklas.grano@gmail.com>
2011-01-25 04:28:38 -0800

    add trackerApp skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrackerApp::Application.config.secret_token = '627fc33c78440c2d3b883c215ffb04ac351a496910f879ef462ea655501559f9e0dedf130e61fa2722bd7220a2212c4f4a55a893d90ebb47fcda1483647c0c75'
