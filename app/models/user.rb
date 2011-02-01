11630f872c65bb38492665a3fcc38f763929bbd4
Niklas Granö <niklas.grano@gmail.com>
2011-02-01 12:53:30 +0200

    Add home controller, modified route file and fixed user model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :friendships
   has_many :friends, :through => :friendships
   
   # Include default devise modules. Others available are:
--- config/routes.rb
+++ config/routes.rb
@@ -51,6 +51,7 @@ TrackerApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+    root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
