c0e54c7d64adf58351e2e4b797a40d6bb6ce2688
Simo Niemelä <simo.niemela@gmail.com>
2011-02-01 19:34:15 +0200

    Add a relation of user and location models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_many :friendships
   has_many :friends, :through => :friendships
-  
+  has_many :locations
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
